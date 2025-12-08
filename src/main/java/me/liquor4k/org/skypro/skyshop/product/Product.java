4ecfb3aa6bd50ae1d064c792696051888594cece
Liquor4k993 <kiratuber01@gmail.com>
2025-12-08 19:53:07 +0300

    выполнение домашнего задания

--- src/main/java/me/liquor4k/org/skypro/skyshop/product/Product.java
+++ src/main/java/me/liquor4k/org/skypro/skyshop/product/Product.java
@@ -1,4 +1,36 @@
 package me.liquor4k.org.skypro.skyshop.product;
 
+/**
+ * Класс, представляющий товар в интернет-магазине.
+ * Значения товара неизменяемы после создания.
+ */
 public class Product {
+    private final String name;
+    private final int price;
+
+    /**
+     * Конструктор товара
+     * @param name название товара
+     * @param price цена товара в рублях
+     */
+    public Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    /**
+     * Получить название товара
+     * @return название товара
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Получить цену товара
+     * @return цена товара
+     */
+    public int getPrice() {
+        return price;
+    }
 }
